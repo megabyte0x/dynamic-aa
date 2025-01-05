790bd28c3fa7a36e34afbb26d85eb6576ac5ae92
megabyte0x <79016290+megabyte0x@users.noreply.github.com>
2025-01-06 02:07:21 +0530

    fix: build

--- webpage/vite.config.js
+++ webpage/vite.config.js
@@ -4,5 +4,23 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  assetsInclude: ['**/*.html'],
+  assetsInclude: ['src/assets/**/*'],
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+    rollupOptions: {
+      output: {
+        format: 'es',
+        entryFileNames: 'assets/[name].[hash].js',
+        chunkFileNames: 'assets/[name].[hash].js',
+        assetFileNames: 'assets/[name].[hash].[ext]'
+      }
+    },
+    manifest: true,
+    sourcemap: true,
+    minify: 'esbuild'
+  },
+  server: {
+    port: 3000
+  }
 })
